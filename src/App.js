46a04cbc2c60d79a817b4fe00a24abd3d0cecee2
Bui Gábor <bui.gabor97@gmail.com>
2021-04-27 08:27:22 +0200

    Add feature to load shapefiles in

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 function App() {
 
   return (
-    <div id='viewDiv'>
+    <div id="viewDiv">
+      <div style={{background: '#fff'}} id="mainWindow">
+        <div>
+          <div>
+            <p>Add a zipped shapefile to the map.</p>
+            <form encType="multipart/form-data" method="post" id="uploadForm">
+              <div className="field">
+                <label className="file-upload">
+                  <span>
+                    <strong>Add File</strong>
+                  </span>
+                  <input type="file" name="file" id="inFile" />
+                </label>
+              </div>
+            </form>
+            <span
+              className="file-upload-status"
+              id="upload-status"
+            ></span>
+            <div id="fileInfo"> </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
